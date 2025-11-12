858bbc1a506e7acd39171c8204ba5ca7f42ddd49
MaVi <131026303+302781@users.noreply.github.com>
2025-11-12 14:30:39 -0300

    ajustes banner

--- js/fundo.js
+++ js/fundo.js
@@ -1,3 +1,14 @@
-const banner = document.querySelector('.fundo');
 
-banner.style.opacity = '0.5';
+function ajustarAlturaBanner() {
+    const banner = document.getElementById('bannerPizza');
+
+    if (banner) {
+        const alturaJanela = window.innerHeight;
+        const alturaDesejada = alturaJanela * 0.6;
+
+        banner.style.height = `${alturaDesejada}px`;
+    }
+}
+
+window.addEventListener('load', ajustarAlturaBanner);
+window.addEventListener('resize', ajustarAlturaBanner);
